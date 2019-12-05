477334f81f8c24602c91c4e6f63195532a2ea8a0
Ian <ijtg.ian@gmail.com>
2019-12-04 21:37:13 -0300

    Firebase Integrado

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -9,10 +9,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { firebase } from 'src/environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore'; //O Database 
+import { AngularFireStorageModule } from '@angular/fire/storage'; 
+import { AngularFireAuthModule } from '@angular/fire/auth'; //Autenticação
+import { AngularFireModule } from '@angular/fire';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+    AngularFireStorageModule, // imports firebase/storage only needed for storage features
+    AngularFireModule.initializeApp(firebase.firebase)],
   providers: [
     StatusBar,
     SplashScreen,
--- App/src/environments/environment.ts
+++ App/src/environments/environment.ts
@@ -2,9 +2,20 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+export const firebase = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDXS_5AlNSlDfP_GW4KzXmYro07lWIlMwE',
+    authDomain: "talk2autism-bf7b7.firebaseapp.com",
+    databaseURL: "https://talk2autism-bf7b7.firebaseio.com",
+    projectId: "talk2autism-bf7b7",
+    storageBucket: "talk2autism-bf7b7.appspot.com",
+    messagingSenderId: "202497686840",
+    appId: "1:202497686840:web:6529cae2440cc81b9f15cd",
+    measurementId: "G-D57YE3TPJ7"
+  }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
