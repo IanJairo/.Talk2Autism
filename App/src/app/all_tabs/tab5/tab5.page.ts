7da265ff4fc266380882760274cb872ff5c6a483
Júlia Dias <juliia.dias00@gmail.com>
2020-04-13 00:28:40 -0300

    Tab 5 - FrontEnd
    Terminado o design inicial da página de visualização de mensagens

--- App/src/app/all_tabs/tab5/tab5.page.ts
+++ App/src/app/all_tabs/tab5/tab5.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/app/services/login/login.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab5',
@@ -8,9 +9,46 @@ import { LoginService } from 'src/app/services/login/login.service';
 })
 export class Tab5Page implements OnInit {
 
+  //mensagensRecebidas: Observable<any[]>; // Só declaração de uma lista de variáveis
+  //mensagensEnviadas: Observable<any[]>; // Só declaração de uma lista de variáveis
+
+  mensagensApresentadas: String = "recebidos";
+
+  mensagensRecebidas = [{
+    remetente: 'José@gmail.com',
+    assunto: 'Teste',
+    mensagem: 'Esta é uma mensagem de teste'
+  },
+  {
+    remetente: 'José@gmail.com',
+    assunto: 'Teste',
+    mensagem: 'Esta é uma mensagem de teste'
+  }]
+
+  mensagensEnviadas = [{
+    remetente: 'José@gmail.com',
+    assunto: 'Teste',
+    mensagem: 'Esta é uma mensagem de teste'
+  },
+  {
+    remetente: 'José@gmail.com',
+    assunto: 'Teste',
+    mensagem: 'Esta é uma mensagem de teste'
+  }]
+
   constructor(
     public login: LoginService) {
 
+  }
+
+  segmentChanged(event) {
+    //console.log(event.detail.value); -> Conseguir o valor selecionado
+    this.mensagensApresentadas = event.detail.value;
+
+  }
+
+  novaMensagem(){
+    console.log("nova mensagem")
   }
 
   // Sai do usuário logado
